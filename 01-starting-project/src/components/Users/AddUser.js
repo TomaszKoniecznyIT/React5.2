523b4e5671d0284f710ecdad2b505e0194ee0814
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-07-29 11:48:24 +0100

    menaging the user input state

--- 01-starting-project/src/components/Users/AddUser.js
+++ 01-starting-project/src/components/Users/AddUser.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 const AddUser = (promps) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUsername, enteredAge);
   };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
-        <leble htmlFor="username">Username</leble>
-        <input id="username" type="text"></input>
-        <leble htmlFor="age ">Age (Years)</leble>
-        <input id="age" type="number"></input>
-        <button type="submit">Add User</button>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          onChange={usernameChangeHandler}
+        ></input>
+        <label htmlFor="age ">Age (Years)</label>
+        <input id="age" type="number" onChange={ageChangeHandler}></input>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
